37a75371e50adb9237b1db4c6ef2e31ae8651e82
nonoll <hkroh.nonoll@gmail.com>
2023-07-22 12:57:18 +0900

    00011-easy-tuple-to-object

---
+++ questions/__00011-easy-tuple-to-object/nonoll.ts
@@ -0,0 +1,39 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const tupleNumber = [1, 2, 3, 4] as const
+const tupleMix = [1, '2', 3, '4'] as const
+
+type TupleToObject<T extends readonly PropertyKey[]> = {
+  [P in T[number]]: P
+}
+
+type Resolve1 = TupleToObject<typeof tuple>
+//     ^?
+
+type Resolve2 = TupleToObject<typeof tupleNumber>
+//     ^?
+
+type Resolve3 = TupleToObject<typeof tupleMix>
+//     ^?
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>
+
+type cases = [
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: 'tesla'
+        'model 3': 'model 3'
+        'model X': 'model X'
+        'model Y': 'model Y'
+      }
+    >
+  >,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<
+    Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>
+  >,
+]
